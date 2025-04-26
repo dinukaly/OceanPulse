ed899b24a34085606455b582d2a7336f7d31af6a
Dinuka Liyanage <infodinukalk@gmail.com>
2025-04-26 21:44:31 +0530

    register user

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,1 +1,23 @@
 const User = require("../model/UserSchema");
+
+const registerUser = async (req, res) => {
+    const { username, firstName, lastName, email, dateOfBirth, password, phone, language, boatId, emergencyContact } = req.body;
+    try {
+        const newUser = new User({
+            username,
+            firstName,
+            lastName,
+            email,
+            dateOfBirth,
+            password,
+            phone,
+            language,
+            boatId,
+            emergencyContact
+        });
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully", user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: "Error registering user", error });
+    }
+};
